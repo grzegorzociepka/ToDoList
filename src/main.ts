d0c548080ca3342fa08d94061abd2222c9217637
Grzegorz Ociepka <grzegorzociepka09@gmail.com>
2019-03-24 14:23:15 +0100

    gitignore update, config relocation

--- src/main.ts
+++ src/main.ts
@@ -3,41 +3,11 @@ import App from './App.vue'
 import '../public/style/homepage.scss'
 import router from './router'
 import store from './store'
-import firebase from 'firebase'
-
+import './config/firebase.ts'
 
 Vue.config.productionTip = false
 
 require('dotenv').config()
-
-// Initialize Firebase
-
-var config = {
-  apiKey: "AIzaSyAnxalfNiEy-HJEs0fmLtCsp_InQwdJ67A",
-  authDomain: "todolist-a321d.firebaseapp.com",
-  databaseURL: "https://todolist-a321d.firebaseio.com",
-  projectId: "todolist-a321d",
-  storageBucket: "todolist-a321d.appspot.com",
-  messagingSenderId: "423713761897"
-};
-
-firebase.initializeApp(config);
-/*
-var config = {
-  apiKey: "AIzaSyAnxalfNiEy-HJEs0fmLtCsp_InQwdJ67A",
-  authDomain: "todolist-a321d.firebaseapp.com",
-  databaseURL: "https://todolist-a321d.firebaseio.com",
-  projectId: "todolist-a321d",
-  storageBucket: "todolist-a321d.appspot.com",
-  messagingSenderId: "423713761897"
-
-  apiKey: process.env.VUE_APP_apiKey,
-  authDomain: process.env.VUE_APP_authDomain,
-  databaseURL: process.env.VUE_APP_databaseURL,
-  projectId: process.env.VUE_APP_projectId,
-  storageBucket: process.env.VUE_APP_storageBucket,
-  messagingSenderId: process.env.VUE_APP_messagingSenderId
-};*/
 
 new Vue({
   router,
